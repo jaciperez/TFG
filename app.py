fcec5f75ed6df2af13b9938531cc6c825d635216
unknown <jpo1002@alu.ubu.es>
2025-05-02 12:42:25 +0200

    Integración completa de algunos campos del volante con la base de datos

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, render_template, request, redirect
 from database import db
+from models import Volante
 import os
 
 # Creamos la app primero
@@ -24,6 +25,47 @@ def menu():
 @app.route('/volante')
 def volante():
     return render_template('index.html')
+
+# donde se guardan los datos del formulario en la base de datos
+@app.route('/guardar_volante', methods=['POST'])
+def guardar_volante():
+    datos = request.form
+
+    nuevo_volante = Volante(
+        citado=datos.get('citado'),
+        fecha=datos.get('fecha'),
+        hora=datos.get('hora'),
+        hab=datos.get('hab'),
+        ext=datos.get('ext'),
+        enfermera=datos.get('enfermera'),
+        numero=datos.get('numero'),
+        box=datos.get('box'),
+
+        diabetes='diabetes' in datos,
+        renal='renal' in datos,
+        alergia_yodo='alergia_yodo' in datos,
+        cardiopata='cardiopata' in datos,
+
+        insulina='insulina' in datos,
+        ADO='ADO' in datos,
+        antibioticos='antibioticos' in datos,
+        corticoides='corticoides' in datos,
+
+        estadificacion='estadificacion' in datos,
+        respuesta_a_tto='respuesta_a_tto' in datos,
+        control_evolutivo='control_evolutivo' in datos,
+        TOD='TOD' in datos,
+
+        planificacion_rt='planificacion_rt' in datos,
+        re_estadistificacion='re_estadistificacion' in datos,
+        sospecha_recidiva='sospecha_recidiva' in datos,
+        sospecha_infeccion='sospecha_infeccion' in datos
+    )
+
+    db.session.add(nuevo_volante)
+    db.session.commit()
+
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,1 +1,39 @@
+from database import db
+
+class Volante(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    # Evaluación
+    citado = db.Column(db.String(100))
+    fecha = db.Column(db.String(20))
+    hora = db.Column(db.String(20))
+    hab = db.Column(db.String(50))
+    ext = db.Column(db.String(50))
+    enfermera = db.Column(db.String(100))
+    numero = db.Column(db.String(50))
+    box = db.Column(db.String(50))
+
+    # Antecedentes personales
+    diabetes = db.Column(db.Boolean)
+    renal = db.Column(db.Boolean)
+    alergia_yodo = db.Column(db.Boolean)
+    cardiopata = db.Column(db.Boolean)
+
+    # Medicación actual
+    insulina = db.Column(db.Boolean)
+    ADO = db.Column(db.Boolean)
+    antibioticos = db.Column(db.Boolean)
+    corticoides = db.Column(db.Boolean)
+
+    # Motivo de exploración
+    estadificacion = db.Column(db.Boolean)
+    respuesta_a_tto = db.Column(db.Boolean)
+    control_evolutivo = db.Column(db.Boolean)
+    TOD = db.Column(db.Boolean)
+
+    # Motivo de exploración pt2
+    planificacion_rt = db.Column(db.Boolean)
+    re_estadistificacion = db.Column(db.Boolean)
+    sospecha_recidiva = db.Column(db.Boolean)
+    sospecha_infeccion = db.Column(db.Boolean)
 
